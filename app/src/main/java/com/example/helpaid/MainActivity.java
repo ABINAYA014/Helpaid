8a1016919f823fee4421adde4847e451fb227d3b
ABINAYA014 <43909086+ABINAYA014@users.noreply.github.com>
2020-09-24 14:19:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/helpaid/MainActivity.java
+++ app/src/main/java/com/example/helpaid/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Splash screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
